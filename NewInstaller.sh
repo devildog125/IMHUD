71754eb165514e91a27ce79551b96e31a7d57c4f
Tyler <devildog125@gmail.com>
2018-07-29 15:28:04 -0500

    no message

--- NewInstaller.sh
+++ NewInstaller.sh
@@ -8,7 +8,9 @@ echo -e "Please disable login over serial in raspi-config and enable hardware se
 # INSTALLS AND UPDATES REAL VNC SERVER ALONG WITH PYSOCKS
 echo -e "Fixing VNC and PYSocks warning issue...\\n" 
 sudo apt-get install realvnc-vnc-server && sudo apt-get install realvnc-vnc-viewer -y
-sudo pip install PySocks 
+sudo pip --upgrade pip
+sudo pip install PySocks
+sudo pip install rgbxy & pip install phue
 clear 
 
 cd ~
@@ -24,12 +26,20 @@ echo -e "Cloud Speech copied!\\n"
 sudo cp AIY-wallpaper.png /usr/share/pixel-wallpaper/AIY-wallpaper.png
 echo -e "New wallpaper added!\\n"
 
+clear 
+
+echo -e "Swapping Bluetooth Ports!\\n"
 sudo systemctl stop serial-getty@ttyS0.service
 sudo systemctl disable serial-getty@ttyS0.service
+clear 
 
+echo -e "Copying needed Files!\\n"
 sudo cp action.py ~/voice-recognizer-raspi/src/action.py
 echo -e "Copied actions!\\n"
 sudo cp voice-recognizer-shell.sh ~/bin/voice-recognizer-shell.sh
 sudo cp modules ~/etc/modules
+
+clear 
+echo -e "Installing Processing Now!\\n"
 cd 
 curl https://processing.org/download/install-arm.sh | sudo sh
